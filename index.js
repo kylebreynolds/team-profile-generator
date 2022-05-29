12079ae3afe5082c6a913b0a068847fa93a069ea
Kyle Reynolds <kbreyn06@gmail.com>
2022-05-29 10:51:20 -0500

    test directories setup, not functioning

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generatePage = require('./src/page-template');
 
+//const Manager = require('./lib/Manager');
+//const Employee = require('./lib/Employee');
+//const Intern = require('./lib/Intern');
+//const Engineer = require('./lib/Engineer');
 
 
 
